f02bf35ff399d728cf0e84b689413826c795cde5
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2023-09-29 17:19:07 +0100

    Backend Views.py Edit

--- Backend/chrome_extension_api/screen_recorder/views.py
+++ Backend/chrome_extension_api/screen_recorder/views.py
@@ -25,17 +25,4 @@ class RecordedVideo(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class RecordVideo(viewsets.ViewSet):
-    parser_classes = [MultiPartParser, FormParser]
-
-    """A simple viewset for listing all videos."""
-    def list(self, request):
-        queryset = ScreenRecorder.objects.all()
-        serializer = ScreenRecorderSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        video_uploaded = request.FILES.get('video_file')
-        content_type = video_uploaded.content_type
-        return Response()
 
