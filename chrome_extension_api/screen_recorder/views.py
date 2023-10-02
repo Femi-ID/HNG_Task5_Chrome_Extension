545d5b76d577aec1954f2021315fb625026f99af
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2023-10-02 16:28:06 +0100

    Updated views.py

--- chrome_extension_api/screen_recorder/views.py
+++ chrome_extension_api/screen_recorder/views.py
@@ -10,6 +10,7 @@ import os
 import tempfile
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import base64
+from io import BytesIO
 
 
 class RecordedVideo(APIView):
@@ -30,34 +31,34 @@ class RecordedVideo(APIView):
             # if not video_file:
             #     return Response({"error": "Video file must be provided"}, status=status.HTTP_400_BAD_REQUEST)
 
+
+            # Create a temporary file to store the video chunks
+            # temp_video = tempfile.NamedTemporaryFile(delete=False)
+            # temp_video.write(video_chunk)
+
             # TODO: Tell the FE to send the video in chunks periodically
             video_chunk = request.data.get('video_chunk')
             video_chunk = base64.b64decode(video_chunk)
 
-            # Create a temporary file to store the video chunks
-            temp_video = tempfile.NamedTemporaryFile(delete=False)
-
-            # Append the chunk to the temporary file
-            temp_video.write(video_chunk)
+            temp_video = BytesIO(video_chunk)
 
             # To check if it is the last chunk
             if 'last_chunk' in request.data and request.data['last_chunk']:
                 # Close the temporary file
-                temp_video.close()
+                # temp_video.close()
 
                 # Create a video object and save to thr database
-                video = ScreenRecorder()
-                video.video_file.save(temp_video.name, InMemoryUploadedFile(
-                    file=open(temp_video.name, 'rb'),
-                    field_name='video_file',
-                    name=os.path.basename(temp_video.name),
-                    content_type='video/mp4',
-                    size=os.path.getsize(temp_video.name),
-                    charset=None
-                ))
-                # video.video_id = str(uuid.uuid4())
+                video = ScreenRecorder.objects.create()
+                video_path = os.path.join(settings.MEDIA_ROOT, video.id + '.mp4')
+
+                with open(video_path, 'ab') as video_file_path:
+                    # Append the chunk to the temporary file
+                    video_file_path.write(temp_video.read())
+
+                video.video_file = video_file_path
                 video.save()
 
+                # Clear the temporary file
                 os.remove(temp_video.name)
 
                 serializer = ScreenRecorderSerializer(video)
