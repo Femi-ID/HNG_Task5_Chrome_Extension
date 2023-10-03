d3931ba4324b9d3322518ae509e6075f78977c7d
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2023-10-03 22:22:30 +0100

    Updated views, models, serializers-2

--- Backend/chrome_extension_api/screen_recorder/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import ScreenRecorder
-
-
-class ScreenRecorderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ScreenRecorder
-        fields = '__all__'
-
